5b763607445575b0a7b98352866e0f33a6f0f539
ashishraman96 <ashishraman81@gmail.com>
2017-08-09 23:39:58 +0530

    Get result status from web

---
+++ loops.py
@@ -0,0 +1,3 @@
+days=["Mon","Tue","Wed","Thu", "Fri", "Sat","Sun"]
+for i, d in enumerate(days):
+    print i,d
---
+++ web.py
@@ -0,0 +1,3 @@
+import urllib2
+webUrl = urllib2.urlopen("http://google.co.in")
+print "result code: " + str(webUrl.getcode())
