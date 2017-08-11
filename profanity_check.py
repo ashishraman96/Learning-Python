86d968a172ec2be4c3a8b3a4f3c2816581ea7fe9
ashishraman96 <ashishraman81@gmail.com>
2017-08-11 22:46:22 +0530

    Optimise code

--- profanity_check.py
+++ profanity_check.py
@@ -7,8 +7,11 @@ def read_text():
 def profanity_check(query):
     connection = urllib2.urlopen("http://www.wdylike.appspot.com/?q="+query);
     response = connection.read();
-    if ( response == 'true'):
+    if 'true' in response:
         print ("Profanity Alert!!")
-    else:
+    elif 'false' in response:
         print ("No curse words found")
+    else:
+        print ("Could not scan document")
+    connection.close()
 read_text()
