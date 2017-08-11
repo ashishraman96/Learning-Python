1b7d81e483f831e78b39ea02cbdecf0d24e439d5
ashishraman96 <ashishraman81@gmail.com>
2017-08-11 20:49:34 +0530

    Draw circle from squares

--- square.py
+++ square.py
@@ -1,16 +1,16 @@
 import turtle
-def draw_square():
+def draw_square(new_square):
+
+    for i in range(1,5):
+        new_square.forward(100)
+        new_square.right(90)
+
+def draw_circle():
     window=turtle.Screen()
     window.bgcolor("yellow")
     square= turtle.Turtle()
-    square.forward(100)
-    square.right(90)
-    square.forward(100)
-    square.right(90)
-    square.forward(100)
-    square.right(90)
-    square.forward(100)
-    square.right(90)
+    for i in range(1,37):
+        draw_square(square)
+        square.right(10)
     window.exitonclick()
-
-draw_square()
+draw_circle()
